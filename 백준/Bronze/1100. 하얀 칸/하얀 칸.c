8a24e29ec4e8963b96386e2e6b9a6490a7883e5a
asdf <126747138+pauseaq@users.noreply.github.com>
2023-09-20 12:01:06 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.c
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char chess[8][8];
-    int count = 0;
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            scanf("%c", &chess[i][j]);
-            if (chess[i][j] == 'F') {
-                if ((i == 0 || i == 2 || i == 4 || i == 6) && (j == 0 || j == 2 || j == 4 || j == 6)) {
-                    count++;
-                } else if ((i == 1 || i == 3 || i == 5 || i == 7) && (j == 1 || j == 3 || j == 5 || j == 7)) {
-                    count++;
-                }
-            }
-        }
-        getchar();
-    }
-    printf("%d\n", count);
+	char chessboard[8][8];
+	int count = 0;
+	for (int i = 0; i < 8; i++) scanf("%s", chessboard[i]);
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i + j) % 2 == 0 && chessboard[i][j] == 'F')
+				count++;
+		}
+	} printf("%d\n", count);
 }
