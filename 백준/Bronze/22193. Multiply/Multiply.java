1b57ab6bebbc748fa6d7d9493cdeec40f3eb13e6
asdf <126747138+pauseaq@users.noreply.github.com>
2023-09-30 16:47:21 +0900

    [Bronze V] Title: Multiply, Time: 208 ms, Memory: 17732 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+	static public void main(String []args) {
+        long n, m, a, b;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextLong();
+        m = sc.nextLong();
+        a = sc.nextLong();
+        b = sc.nextLong();
+        System.out.println(a * b);
+    }
+}
