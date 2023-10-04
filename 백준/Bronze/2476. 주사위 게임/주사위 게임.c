6d61e6343fcf8a836937ac920b0e77fba351eb72
asdf <126747138+pauseaq@users.noreply.github.com>
2023-10-04 12:06:32 +0900

    [Bronze III] Title: 주사위 게임, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.c
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.c
@@ -5,19 +5,32 @@ int main() {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d %d %d", &a, &b, &c);
-        if (a == b && b == c) sum[i] += 10000 + (a * 1000);
-        else if (a == b && b != c) sum[i] += 1000 + (a * 100);
-        else if (b == c && c != a) sum[i] += 1000 + (b * 100);
-        else if (c == a && a != b) sum[i] += 1000 + (c * 100);
-        else {
-            if (a < b && b < c) sum[i] += c * 100;
-            else if (b < a && a < c) sum[i] += c * 100;
-            else if (b < c && c < a) sum[i] += a * 100;
-            else if (c < b && b < a) sum[i] += a * 100;
-            else if (c < a && a < b) sum[i] += b * 100;
-            else if (a < c && c < b) sum[i] += b * 100;
+        if (a == b && b == c) {
+            sum[i] += 10000 + (a * 1000);
+        } else if (a == b && b != c) {
+            sum[i] += 1000 + (a * 100);
+        } else if (b == c && c != a) {
+            sum[i] += 1000 + (b * 100);
+        } else if (c == a && a != b) {
+            sum[i] += 1000 + (c * 100);
+        } else {
+            if (a < b && b < c) {
+                sum[i] += c * 100;
+            } else if (b < a && a < c) {
+                sum[i] += c * 100;
+            } else if (b < c && c < a) {
+                sum[i] += a * 100;
+            } else if (c < b && b < a) {
+                sum[i] += a * 100;
+            } else if (c < a && a < b) {
+                sum[i] += b * 100;
+            } else if (a < c && c < b) {
+                sum[i] += b * 100;
+            }
         }
     }
-    for (int i = 0; i < n; i++) if (max < sum[i]) max = sum[i];
+    for (int i = 0; i < n; i++) {
+        if (max < sum[i]) max = sum[i];
+    }
     printf("%d\n", max);
 }
