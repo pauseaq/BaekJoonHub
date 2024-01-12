e72eb0cbeea99d70fbe5734e574fa541a800c9ba
asdf <126747138+pauseaq@users.noreply.github.com>
2024-01-13 08:49:52 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, num, sum = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        sum += num;
+    }
+    printf("%d\n", sum);
+}
