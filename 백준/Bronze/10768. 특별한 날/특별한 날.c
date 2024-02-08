b63de74e9e4f0260d2c18575976312eadac22019
asdf <126747138+pauseaq@users.noreply.github.com>
2024-02-08 10:25:08 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int m, d;
+    scanf("%d %d", &m, &d);
+    if (m < 2) printf("Before\n");
+    else if (m > 2) printf("After\n");
+    else {
+        if (d < 18) printf("Before\n");
+        else if (d > 18) printf("After\n");
+        else printf("Special\n");
+    }
+}
